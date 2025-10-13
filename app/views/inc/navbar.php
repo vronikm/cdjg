19f0ccbb2221c2a6df7e1aee88953e110014a197
vronik <veronicaquinde@digitech.com.ec>
2025-10-13 17:37:12 -0500

    Restringir acceso a rol profesor a menu superior

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -5,15 +5,18 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href=<?php echo APP_URL."alumnoNew/";?> class="nav-link ">Nuevo alumno</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href=<?php echo APP_URL."alumnoList/";?> class="nav-link ">Buscar alumno</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href=<?php echo APP_URL."pagosList/";?> class="nav-link ">Registrar pago</a>
-        </li>    
+
+         <?php if ($_SESSION['rol'] <> 3): ?>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href=<?php echo APP_URL."alumnoNew/";?> class="nav-link ">Nuevo alumno</a>
+            </li>             
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href=<?php echo APP_URL."alumnoList/";?> class="nav-link ">Buscar alumno</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href=<?php echo APP_URL."pagosList/";?> class="nav-link ">Registrar pago</a>
+            </li> 
+        <?php endif; ?>   
     </ul>
 
     <!-- Right navbar links -->
